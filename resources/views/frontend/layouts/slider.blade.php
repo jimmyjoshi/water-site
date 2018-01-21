32ab9393523f2cde85c8088ff82c227bb87b5470
Anuj Jaha <er.anujjaha@gmail.com>
2018-01-22 00:24:46 +0530

    water slider images

--- resources/views/frontend/layouts/slider.blade.php
+++ resources/views/frontend/layouts/slider.blade.php
@@ -6,7 +6,7 @@
 
 			<!-- BEGIN .Slide 1 -->
 			<li data-transition="fade">
-				<img src="images/image15.jpg"  alt=""  width="1920" height="1280">
+				<img src="{{ URL::to('/').'/uploads/slider/1.jpeg'}}"  alt=""  width="1920" height="1280">
 				<div class="tp-caption rev-custom-caption-1"
 					data-x="center"
 					data-y="center"
@@ -30,7 +30,79 @@
 			
 			<!-- BEGIN .Slide 1 -->
 			<li data-transition="fade">
-				<img src="images/image15.jpg"  alt=""  width="1920" height="1280">
+				<img src="{{ URL::to('/').'/uploads/slider/2.jpeg'}}"  alt=""  width="1920" height="1280">
+				<div class="tp-caption rev-custom-caption-2"
+					data-x="left"
+					data-y="center"
+					data-whitespace="normal"
+					data-transform_idle="o:1;"
+					data-transform_in="o:0"
+					data-transform_out="o:0"
+					data-start="500">
+					<div class="rev-caption-wrapper clearfix">
+						<h3>Great Sailing Experiences</h3>
+						<div class="title-block1"></div>
+						<p>Enjoy sailing in pristine conditions, it doesn't matter what your experience<br /> level is, we have a skilled crew to assist you</p>
+						<div class="slideshow-button-wrapper">
+							<a href="photos-4-col.html" class="slideshow-button-photos"><i class="fa fa-image"></i>Photos</a>
+							<a href="about-us.html" class="slideshow-button-about"><i class="fa fa-info-circle"></i>About Us</a>
+						</div>
+					</div>
+				</div>
+			<!-- END .Slide 1 -->
+			</li>
+
+			<!-- BEGIN .Slide 1 -->
+			<li data-transition="fade">
+				<img src="{{ URL::to('/').'/uploads/slider/3.jpeg'}}"  alt=""  width="1920" height="1280">
+				<div class="tp-caption rev-custom-caption-2"
+					data-x="left"
+					data-y="center"
+					data-whitespace="normal"
+					data-transform_idle="o:1;"
+					data-transform_in="o:0"
+					data-transform_out="o:0"
+					data-start="500">
+					<div class="rev-caption-wrapper clearfix">
+						<h3>Great Sailing Experiences</h3>
+						<div class="title-block1"></div>
+						<p>Enjoy sailing in pristine conditions, it doesn't matter what your experience<br /> level is, we have a skilled crew to assist you</p>
+						<div class="slideshow-button-wrapper">
+							<a href="photos-4-col.html" class="slideshow-button-photos"><i class="fa fa-image"></i>Photos</a>
+							<a href="about-us.html" class="slideshow-button-about"><i class="fa fa-info-circle"></i>About Us</a>
+						</div>
+					</div>
+				</div>
+			<!-- END .Slide 1 -->
+			</li>
+
+			<!-- BEGIN .Slide 1 -->
+			<li data-transition="fade">
+				<img src="{{ URL::to('/').'/uploads/slider/4.jpeg'}}"  alt=""  width="1920" height="1280">
+				<div class="tp-caption rev-custom-caption-2"
+					data-x="left"
+					data-y="center"
+					data-whitespace="normal"
+					data-transform_idle="o:1;"
+					data-transform_in="o:0"
+					data-transform_out="o:0"
+					data-start="500">
+					<div class="rev-caption-wrapper clearfix">
+						<h3>Great Sailing Experiences</h3>
+						<div class="title-block1"></div>
+						<p>Enjoy sailing in pristine conditions, it doesn't matter what your experience<br /> level is, we have a skilled crew to assist you</p>
+						<div class="slideshow-button-wrapper">
+							<a href="photos-4-col.html" class="slideshow-button-photos"><i class="fa fa-image"></i>Photos</a>
+							<a href="about-us.html" class="slideshow-button-about"><i class="fa fa-info-circle"></i>About Us</a>
+						</div>
+					</div>
+				</div>
+			<!-- END .Slide 1 -->
+			</li>
+
+			<!-- BEGIN .Slide 1 -->
+			<li data-transition="fade">
+				<img src="{{ URL::to('/').'/uploads/slider/5.jpeg'}}"  alt=""  width="1920" height="1280">
 				<div class="tp-caption rev-custom-caption-2"
 					data-x="left"
 					data-y="center"
