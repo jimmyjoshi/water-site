6673a1f8923215140d388432b27e61f315306d99
Anuj Jaha <er.anujjaha@gmail.com>
2018-01-24 23:00:53 +0530

    footer page

--- resources/views/frontend/layouts/bottom-bar.blade.php
+++ resources/views/frontend/layouts/bottom-bar.blade.php
@@ -5,7 +5,7 @@
 			<div class="footer-inner clearfix">
 
 				<!-- BEGIN .one-fourth -->
-				<div class="one-fourth">
+				<div class="qns-one-half">
 
 					<h5>About Us</h5>
 					<div class="title-block6"></div>
@@ -15,54 +15,17 @@
 				<!-- END .one-fourth -->
 				</div>
 
-				<!-- BEGIN .one-fourth -->
-				<div class="one-fourth">
-
-					<h5>Newsletter</h5>
-					<div class="title-block6"></div>
-					<p>Subscribe to our newsletter for news, updates, exclusive discounts and offers.</p>
-
-					<!-- BEGIN .newsletter-form -->
-					<form action="#" class="newsletter-form" method="post">
-
-						<input type="text" name="your-name" value="" />
-						<button type="submit">
-			 				<i class="fa fa-envelope"></i> Subscribe
-						</button>
-
-					<!-- END .newsletter-form -->
-					</form>
-
-				<!-- END .one-fourth -->
-				</div>
+				
 
 				<!-- BEGIN .one-fourth -->
-				<div class="one-fourth">
-
-					<h5>Our Company</h5>
-					<div class="title-block6"></div>
-					<ul>
-						<li><a href="#">About Yacht Charter</a></li>
-						<li><a href="#">Customer Testimonials</a></li>
-						<li><a href="#">British Virgin Islands Trips</a></li>
-						<li><a href="#">Latest Weather Reports</a></li>
-						<li><a href="#">All Inclusive Day Trips</a></li>
-						<li><a href="#">Book Online Now</a></li>
-						<li><a href="#">Get In Touch</a></li>
-					</ul>
-
-				<!-- END .one-fourth -->
-				</div>
-
-				<!-- BEGIN .one-fourth -->
-				<div class="one-fourth">
+				<div class="qns-one-half qns-last">
 
 					<h5>Contact Details</h5>
 					<div class="title-block6"></div>
 					<ul class="contact-widget">
-						<li class="cw-address">1 Roadtown Street, The Yacht Building, British Virgin Islands</li>
-						<li class="cw-phone">1800-1111-2222<span>Mon - Fri, 9.00am until 6.30pm</span></li>
-						<li class="cw-cell">booking@example.com<span>We reply within 24 hrs</span></li>
+						<li class="cw-address">11-B VATSA HOUSE, JANMA BHOOMI MARG, MUMBAI 400001, INDIA</li>
+						<li class="cw-phone">+91 9923 599 203<span>Mon - Fri, 9.00am - 18.30pm</span></li>
+						<li class="cw-cell">kunal@apacintegrated.com<span>We reply within 24 hrs</span></li>
 					</ul>
 
 				<!-- END .one-fourth -->
